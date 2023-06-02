dc9e3a27a63a55a41e4b7304ce0421b175c752db
Jed Laundry <jlaundry@jlaundry.com>
2023-06-02 05:11:44 +0000

    and the other way

--- info/roles.json.py
+++ info/roles.json.py
@@ -7,12 +7,26 @@ with open("roles.json") as of:
 outfile = open("roles.md", "w")
 outfile.write("# AAD Built-In Role Permissions\n\n")
 
+outfile.write("## Permissions by Role\n\n")
 outfile.write("| Role | Permission |\n| ---- | ---------- |\n")
+
+permissions = {}
 
 for role in roles["Roles"]:
     display_name = role['DisplayName']
     for perm in role["RolePermissions"]:
         for action in sorted(perm["ResourceActions"]["AllowedResourceActions"]):
             outfile.write(f"| {display_name} | {action} |\n")
+
+            if action not in permissions.keys():
+                permissions[action] = []
+            permissions[action].append(display_name)
+
+outfile.write("\n\n## Roles by Permission\n\n")
+outfile.write("| Permission | Roles |\n| ---------- | ----- |\n")
+
+for action in permissions.keys():
+    roles = ", ".join(sorted(permissions[action]))
+    outfile.write(f"| {action} | {roles} |\n")
 
 outfile.close()
